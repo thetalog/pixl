e3efcc22b534932c8e78b9eaac95395beb3654d2
Deep <mail30deeprajdas@gmail.com>
2025-04-15 17:02:07 +0530

    update compose file

--- backend/mongo-init/init-replica.js
+++ backend/mongo-init/init-replica.js
@@ -1,7 +1,7 @@
 // mongo-init/init-replica.js
 const conn = new Mongo("mongodb://admin:adminadmin1@mongod1:27017/admin");
 const admin = conn.getDB("admin");
-admin.auth("admin", "adminadmin1");  // optional if already authenticated
+admin.auth("admin", "adminadmin1"); // optional if already authenticated
 
 rs.initiate({
   _id: "rs0",
@@ -9,6 +9,7 @@ rs.initiate({
     { _id: 0, host: "mongod1:27017" },
     { _id: 1, host: "mongod2:27017" },
     { _id: 2, host: "mongod3:27017" },
-    { _id: 3, host: "mongod4:27017", arbiterOnly: true }
-  ]
+    { _id: 3, host: "mongod4:27017" },
+  ],
 });
+db.getMongo().setReadPref("nearest");
